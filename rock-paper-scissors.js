98061750f6257f7c6dcaa314dfd625d840a12fdb
Rokachello <gabrovsek.rok@hotmail.com>
2023-07-20 22:29:59 +0200

    github test

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,7 +2,7 @@
 // function randomly returns rock/paper/scissors. It simulates computers choice.
 function getComputerChoice(){
 
-    let sign = ["Rock", "Paper", "Scissors"]
+    let symbol = ["Rock", "Paper", "Scissors"]
     let choiceNumber = Math.floor(Math.random()*3)
     
     return sign[choiceNumber]
